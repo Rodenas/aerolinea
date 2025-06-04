12da4efa565e63a68dd55d55c09ea4d931e3a8c8
Rodenas <48320364+Rodenas@users.noreply.github.com>
2025-06-04 16:50:29 -0300

    se edito el modulo de ABM de cliente para que se conecte en la base de datos

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import cliente_db;
 
 """
 Propósito del sistema:
@@ -22,6 +23,9 @@ Datos de los integrantes del grupo:
 | Tissera  | Guillermo Adrián | 35.260.232 |
 --------------------------------------------
 """
+
+
+
 
 # indica el nombre del sistema
 nombre_sistema = 'AirHappy'
@@ -51,9 +55,7 @@ while fin_programa == 'no':
     print('-----------------------------')
     # se le pide al usuario que ingresa una opcion del menu
     opcion_escogida = int(input('Seleccione una opción: '))
-    print('-----------------------------')
-    print('-----------------------------')
-    print('-----------------------------')
+    print('-----------------------------\n' * 3)
 
     # mostrar menu Gestionar Clientes
     if opcion_escogida == 1:
@@ -72,15 +74,10 @@ while fin_programa == 'no':
                 print('-----------------------------')
                 # se le pide al usuario que ingresa una opcion del menu
                 opcion_escogida = int(input('Seleccione una opción: '))
-                print('-----------------------------')
-                print('-----------------------------')
-                print('-----------------------------')
+                print('-----------------------------\n' * 3)
 
-                
+                # mostrar alta de cliente #
                 if opcion_escogida == 1:
-                    ###########################
-                    # mostrar alta de cliente #
-                    ###########################
                     print('###################')
                     print('# Alta de cliente #')
                     print('###################')
@@ -88,131 +85,144 @@ while fin_programa == 'no':
                     # se le pide al usuario que ingrese la razon social del cliente
                     razon_social = input('Ingrese razón social: ')
                     # se le pide al usuario que ingrese el CUIT del cliente
-                    cuit = input('Ingrese CUIT: ')
+                    cuit = int(input('Ingrese CUIT: '))
                     # se le pide al usuario que ingrese el correo electronico del cliente
                     correo = input('Ingrese correo electrónico: ')
-                    print('-----------------------------')
-                    print('-----------------------------')
-                    print('-----------------------------')
+                    print('-----------------------------\n' * 3)
                     # se le muestra al usuario que datos se cargaron
                     print('Los datos del cliente cargado son:')
                     print(f'Razón social: {razon_social}')
                     print(f'CUIT: {cuit}')
                     print(f'Correo electrónico: {correo}')
                     print('-----------------------------')
-                    print('Cliente guardado correctamente')
+                    # comprueba si el cuit ya eta cargado
+                    if cliente_db.cliente_existe(cuit):
+                        print('El cliente ya existe')
+                        print('-----------------------------')
+                        print('Los datos no fueron cargados')
+                    else:
+                        # guarda los datos del cliente en la base de datos
+                        cliente_db.cliente_crear(cuit, razon_social, correo)                        
+                        print('Cliente guardado correctamente')
+
                     print('-----------------------------')
                     input("Presione Enter para volver al menú de cliente...")
-                    print('-----------------------------')
-                    print('-----------------------------')
-                    print('-----------------------------')
+                    print('-----------------------------\n' * 3)
+                    continue
 
+                # mostrar modificar cliente #
                 if opcion_escogida == 2:
-                    #############################
-                    # mostrar modificar cliente #
-                    #############################
                     print('#####################')
                     print('# Modificar cliente #')
                     print('#####################')
                     # se le pide al usuario que ingrese el nombre del usuario que quiere modificar
-                    cliente_modificar = input('Ingrese el nombre del cliente a modificar: ')
-                    # se le indica al usuario que el cliente que quiere modificar fue encontrado
-                    print(f'Cliente {cliente_modificar} encontrado')
-                    input("Presione Enter para continuar...")
-                    print('-----------------------------')
-                    # se le pide al usuario que ingrese la razon social del cliente
-                    razon_social = input('Ingrese razón social: ')
-                    # se le pide al usuario que ingrese el cuit del cliente
-                    cuit = input('Ingrese CUIT: ')
-                    # se le pide al usuario que ingrese el correo electronico del cliente
-                    correo = input('Ingrese correo electrónico: ')
-                    print('-----------------------------')
-                    print('-----------------------------')
-                    print('-----------------------------')
-                    # se le muestra al usuario que datos se cargaron
-                    print('Los datos del cliente cargado son:')
-                    print(f'Razón social: {razon_social}')
-                    print(f'CUIT: {cuit}')
-                    print(f'Correo electrónico: {correo}')
-                    print('-----------------------------')
-                    print('Cliente guardado correctamente')
+                    cliente_modificar = int(input('Ingrese el cuit del cliente a modificar: '))
+                    # comprueba si el cliente existe
+                    if cliente_db.cliente_existe(cliente_modificar):
+                        # se le indica al usuario que el cliente que quiere modificar fue encontrado
+                        print(f'Cliente {cliente_modificar} encontrado')
+                        input("Presione Enter para continuar...")
+                        print('-----------------------------')
+                        # se le pide al usuario que ingrese la razon social del cliente
+                        razon_social = input('Ingrese razón social: ')
+                        # se le pide al usuario que ingrese el cuit del cliente
+                        cuit = int(input('Ingrese CUIT: '))
+                        # se le pide al usuario que ingrese el correo electronico del cliente
+                        correo = input('Ingrese correo electrónico: ')
+                        print('-----------------------------\n' * 3)
+                        # se le muestra al usuario que datos se cargaron
+                        print('Los datos del cliente cargado son:')
+                        print(f'Razón social: {razon_social}')
+                        print(f'CUIT: {cuit}')
+                        print(f'Correo electrónico: {correo}')
+                        print('-----------------------------')
+
+                        # comprueba si el cuit ya eta cargado
+                        if cliente_modificar != cuit and cliente_db.cliente_existe(cuit):
+                            print('Ya existe un cliente con ese cuit')
+                            print('-----------------------------')
+                            print('Los datos del cliente no se actualizaron')
+                        else:
+                            # actualiza los datos del cliente en la base de datos
+                            cliente_db.cliente_actualizar(cliente_modificar, cuit, razon_social, correo)
+                            print('Cliente guardado correctamente')
+                    else:
+                        print('-----------------------------')
+                        print('Cliente no encontrado')
+
                     print('-----------------------------')
                     input("Presione Enter para volver al menú de cliente...")
-                    print('-----------------------------')
-                    print('-----------------------------')
-                    print('-----------------------------')
+                    print('-----------------------------\n' * 3)
+                    continue
 
+                # mostrar dar de baja cliente #
                 if opcion_escogida == 3:
-                    ###############################
-                    # mostrar dar de baja cliente #
-                    ###############################
                     print('#######################')
                     print('# Dar de baja cliente #')
                     print('#######################')
                     # se le pide al usuario que ingrese el nombre del usuario que quiere dar de baja
-                    cliente_baja = input('Ingrese el nombre del cliente a dar de baja: ')
-                    print(f'Cliente {cliente_baja} encontrado')
+                    cliente_baja = int(input('Ingrese el cuit del cliente a dar de baja: '))
                     print('-----------------------------')
-                    input("Presione Enter para continuar...")
-                    print('-----------------------------')
-                    # se le indica al usuario que el cliente ingresado fue dado de baja
-                    print(f'El cliente {cliente_baja} fue dado de baja')
+                    # comprueba si el cuit ya eta cargado
+                    if cliente_db.cliente_existe(cliente_baja):
+                        print(f'Cliente {cliente_baja} encontrado')
+                        print('-----------------------------')
+                        input("Presione Enter para continuar...")
+                        print('-----------------------------')
+                        # actualiza los datos del cliente en la base de datos
+                        cliente_db.cliente_anular(cliente_baja)
+                        # se le indica al usuario que el cliente ingresado fue dado de baja
+                        print(f'El cliente {cliente_baja} fue dado de baja')
+                    else:
+                        print(f'El cliente {cliente_baja} no existe')
                     print('-----------------------------')
                     input("Presione Enter para volver al menú de cliente...")
-                    print('-----------------------------')
-                    print('-----------------------------')
-                    print('-----------------------------')
+                    print('-----------------------------\n' * 3)
+                    continue
 
+                # volver al menu principal #
                 if opcion_escogida == 4:
-                    ############################
-                    # volver al menu principal #
-                    ############################
                     print('-----------------------------')
                     print('Volver al menú principal')
-                    print('-----------------------------')
-                    print('-----------------------------')
-                    print('-----------------------------')
+                    print('-----------------------------\n' * 3)
                     continuar_abm_cliente = 'no'
+                    opcion_escogida = 0
+                    continue
 
     # mostrar menu Gestionar Destinos
     if opcion_escogida == 2:
         print('Muestra menú "Gestionar Destinos"')
         input("Presione Enter para volver al menú principal...")
-        print('-----------------------------')
-        print('-----------------------------')
-        print('-----------------------------')
+        print('-----------------------------\n' * 3)
+        continue
 
     # mostrar menu Gestionar Ventas
     if opcion_escogida == 3:
         print('Muestra menú "Gestionar Ventas"')
         input("Presione Enter para volver al menú principal...")
-        print('-----------------------------')
-        print('-----------------------------')
-        print('-----------------------------')
+        print('-----------------------------\n' * 3)
+        continue
 
     # mostrar menu Consultar Ventas
     if opcion_escogida == 4:
         print('Muestra menú "Consultar Ventas"')
         input("Presione Enter para volver al menú principal...")
-        print('-----------------------------')
-        print('-----------------------------')
-        print('-----------------------------')
+        print('-----------------------------\n' * 3)
+        continue
 
     # mostrar menu Botón de Arrepentimiento
     if opcion_escogida == 5:
         print('Muestra menú "Botón de Arrepentimiento"')
         input("Presione Enter para volver al menú principal...")
-        print('-----------------------------')
-        print('-----------------------------')
-        print('-----------------------------')
+        print('-----------------------------\n' * 3)
+        continue
        
     # mostrar menu Ver Reporte General
     if opcion_escogida == 6:
         print('Muestra menú "Ver Reporte General"')
         input("Presione Enter para volver al menú principal...")
-        print('-----------------------------')
-        print('-----------------------------')
-        print('-----------------------------')
+        print('-----------------------------\n' * 3)
+        continue
 
     # mostrar menu Acerca del sistema
     if opcion_escogida == 7:
@@ -241,6 +251,7 @@ while fin_programa == 'no':
         print('| Tissera  | Guillermo Adrián | 35.260.232 |')
         print('--------------------------------------------')
         input("Presione Enter para volver al menú principal...")
+        continue
 
     # mostrar menu Finalizar Programa
     if opcion_escogida == 8:
@@ -255,6 +266,7 @@ while fin_programa == 'no':
             for i in range(10):
                 print('.')
             fin_programa = 'si'
+        continue
 
 
 print("Programa finalizado")
